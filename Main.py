5c60a28b6983d3a73bb8ef4df94ec73111ce6f26
Jefferson <jeff.azevedo@live.com>
2020-12-26 15:23:35 -0300

    Implementation of template method and inheritance for normalization of common methods

--- Main.py
+++ Main.py
@@ -1,23 +1,28 @@
-from Tax import ISS, ICMS
+from Tax import ISS, ICMS, ICPP, IKCV
 from Budget import Budget, Item
 from Discount import Discount_Calculator
 from TaxCalculator import TaxCalculator
 
 # First Class Alura
 
-# calculator = TaxCalculator()
+calculator = TaxCalculator()
 # budget = Budget(500)
 #
 # calculator.Calculate(budget, ISS())
 # calculator.Calculate(budget, ICMS())
 
 budget = Budget()
-budget.add_item(Item('ITEM - 1', 100))
-budget.add_item(Item('ITEM - 2', 50))
-budget.add_item(Item('ITEM - 3', 500))
+budget.add_item(Item('ITEM - 1', 50))
+budget.add_item(Item('ITEM - 2', 200))
+budget.add_item(Item('ITEM - 3', 250))
 
+calculator.Calculate(budget, ISS())
+calculator.Calculate(budget, ICMS())
+calculator.Calculate(budget, ICPP())
+calculator.Calculate(budget, IKCV())
 
-calculator = Discount_Calculator()
-calculated_discount = calculator.Calculate(budget)
+#
+# calculator = Discount_Calculator()
+# calculated_discount = calculator.Calculate(budget)
 
-print(f'Calculated discount is :{round(calculated_discount, 2)}')
+# print(f'Calculated discount is :{round(calculated_discount, 2)}')
--- Tax.py
+++ Tax.py
@@ -1,3 +1,27 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ConditionalTaxTemplate(object, metaclass=ABCMeta):
+
+    def Calculate(self, budget):
+        if self.should_use_max_tax(budget):
+            return self.max_tax(budget)
+        else:
+            return self.min_tax(budget)
+
+    @abstractmethod
+    def should_use_max_tax(self, budget):
+        pass
+
+    @abstractmethod
+    def max_tax(self, budget):
+        pass
+
+    @abstractmethod
+    def min_tax(self, budget):
+        pass
+
+
 class ISS(object):
     def Calculate(self, budget):
         return budget.value * 0.1
@@ -6,3 +30,32 @@ class ISS(object):
 class ICMS(object):
     def Calculate(self, budget):
         return budget.value * 0.06
+
+
+class ICPP(ConditionalTaxTemplate):
+    def should_use_max_tax(self, budget):
+        return budget.value > 500
+
+    def max_tax(self, budget):
+        return budget.value * 0.07
+
+    def min_tax(self, budget):
+        return budget.value * 0.05
+
+
+class IKCV(ConditionalTaxTemplate):
+    def should_use_max_tax(self, budget):
+        return budget.value > 500 and self.__has_item_100_more_valued(budget)
+
+    def max_tax(self, budget):
+        return budget.value * 0.1
+
+    def min_tax(self, budget):
+        return budget.value * 0.06
+
+    def __has_item_100_more_valued(self, budget):
+        for item in budget.get_items():
+            if item.value > 100:
+                return True
+            else:
+                return False
